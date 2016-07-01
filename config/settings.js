6f2dd6b60d44dc421c9bfbc14170f876dca3582d
e <rainit2006@gmail.com>
2016-07-01 21:48:11 +0900

    first commit

---
+++ config/settings.js
@@ -0,0 +1,2 @@
+exports.port = 30000;
+exports.post = '153.126.131.5';
---
+++ server/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var settings = require(__dirname + '/../config/settings');
+
+app.use(express.static(__dirname + '/../client'));
