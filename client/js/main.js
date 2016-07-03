b30a6a4e439678289814ee416d0cf7c8f13601e7
e <rainit2006@gmail.com>
2016-07-03 11:06:00 +0900

    update css and js codes

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,11 @@
 $(function() {
     var GameItemsArray = [];
-
+    var FiledArray = [];
+    var FiledLength = 10;
+    var Action={//记录玩家的操作
+        index_GameItems:null,
+        index_Filed:null
+    };
 
     window.onload = function(){
         init();
@@ -9,6 +14,9 @@ $(function() {
 
     function init(){
       initGameItems();
+      initFiled();
+      drawGameItemsArea();
+      drawFiledArea();
       bindClickEvent();
 
       console.log("init done.");
@@ -20,7 +28,17 @@ $(function() {
         GameItemsArray.push(["咖啡", 9, 1]);
         GameItemsArray.push(["item3", 3, 1]);
         GameItemsArray.push(["item4", 3, 0]);
+    }
 
+    function initFiled(){
+        FiledArray=[];
+        for(var i = 0; i < FiledLength; i ++){
+            FiledArray.push(["空地", 0, 0 ]); //param： 土地种类，归属者，数量
+        }
+        FiledArray.push(["工厂", 2, 1]);
+    }
+
+    function drawGameItemsArea() {
         var ulNode = $("<ul id='GameItemsUlNode'>");
         $.each(GameItemsArray, function(){
               //ulNode.append("<li>"+this[0]+":"+this[1]+"</li>");
@@ -30,14 +48,55 @@ $(function() {
               }
               ulNode.append(liNode);
         });
-        $("#GameItemsArea").append(ulNode);
+        $("#GameItemsArea").empty().append(ulNode);
     }
+
+    function drawFiledArea(){
+        var ulNode = $("<ul id='FiledAreaNode'>");
+        $.each(FiledArray, function(){
+              //ulNode.append("<li>"+this[0]+":"+this[1]+"</li>");
+              var liNode = $("<li>"+this[0]+"</li>");
+              if(this[1] === 0){
+                  liNode.addClass('vacant');
+              }
+              if(this[1] === 1){
+                  liNode.addClass('player1');
+              }
+              if(this[1] === 2){
+                  liNode.addClass('player2');
+              }
+              ulNode.append(liNode);
+        });
+        $("#FiledArea").empty().append(ulNode);
+    }
+
 
     function bindClickEvent(){
       $('#GameItemsUlNode li').on('click', function(){
           var index = $('#GameItemsUlNode li').index(this);
           console.log(GameItemsArray[index][0]+','+GameItemsArray[index][1] +' is clicked!');
+          Action.index_GameItems = index;
+      });
+
+      $('#FiledAreaNode li').on('click', function(){
+          var index = $('#FiledAreaNode li').index(this);
+          console.log(index+':'+FiledArray[index][0]+','+FiledArray[index][1] +' is clicked!');
+          Action.index_Filed =index;
       });
     }
+
+    $('#SubmitArea input:button').click(function(){
+        //console.log("button clicked");
+        var index_GameItems = Action.index_GameItems;
+        var index_Filed = Action.index_Filed;
+        console.log(index_GameItems+";"+index_Filed);
+
+        GameItemsArray[index_GameItems][1] = GameItemsArray[index_GameItems][1] - 1;
+        FiledArray[index_Filed][0] = GameItemsArray[index_GameItems][0];
+        drawGameItemsArea();
+        drawFiledArea();
+        bindClickEvent();
+
+    });
 
 })
